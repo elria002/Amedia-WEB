3d8e7b07330409be5a31dda979cf7c9cafb9ad1f
elria002 <elias.ringmo@amedia.no>
2024-05-24 12:53:40 +0200

    lage darkmode

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 console.log("heihei");
+console.log("heihei");
+
+const sjekkboksen = document.querySelector("#toggle");
+const heleSiden = document.querySelector("#page");
+
+toggle.addEventListener("click", function (event) {
+  if (toggle.checked) {
+    heleSiden.classList.add("darkmode");
+  } else {
+    heleSiden.classList.remove("darkmode");
+  }
+});
